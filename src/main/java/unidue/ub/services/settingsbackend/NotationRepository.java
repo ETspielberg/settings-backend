c389aec4c8cf7cc8da632610a8157d5a80003255
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-07-21 14:08:19 +0200

    getting notations based on notationgroup

--- src/main/java/unidue/ub/services/settingsbackend/NotationRepository.java
+++ src/main/java/unidue/ub/services/settingsbackend/NotationRepository.java
@@ -2,6 +2,7 @@ package unidue.ub.services.settingsbackend;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import unidue.ub.settings.fachref.Notation;
@@ -12,6 +13,10 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "notation", path = "notation")
 public interface NotationRepository  extends PagingAndSortingRepository<Notation, String>{
 
-    @Query("SELECT p FROM Notation p WHERE SUBSTRING(p.notation,1,3) BETWEEN :start AND :end")
-    public List<Notation> findNotationsBetweenStartAndEnd(String start, String end);
+    @Query(value = "SELECT p.* FROM Notation p WHERE SUBSTRING(p.notation,1,3) BETWEEN :startNotation AND :endNotation", nativeQuery = true)
+    public List<Notation> getNotationList(@Param("startNotation") String startNotation, @Param("endNotation") String endNotation);
+
+    @Query(value = "SELECT n.* FROM Notation n, Notationgroup ng WHERE  ng.notationgroup_name = :notationgroupName AND SUBSTRING(n.notation,1,3) BETWEEN ng.notations_start AND ng.notations_end" , nativeQuery = true)
+    public List<Notation> getNotationListForNotationgroup(@Param("notationgroupName") String notationgroupName);
+
 }
